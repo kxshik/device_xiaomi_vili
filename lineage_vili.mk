04687c06eab7dd760ee1517aab67636e45f31a33
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2025-06-22 16:28:37 +0530

    vili: lineage ->> AxionOS

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -25,3 +25,22 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="vili-user 14 UKQ1.231207.002 V816.0.10.0.UKDMIXM release-keys" \
     BuildFingerprint=Xiaomi/vili/vili:14/UKQ1.231207.002/V816.0.10.0.UKDMIXM:user/release-keys
+
+# Define rear camera specs
+AXION_CAMERA_REAR_INFO := 108,8,5
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 16
+
+# Maintainer name
+AXION_MAINTAINER := swiitchOFF
+
+# Processor name
+AXION_PROCESSOR := Qualcomm_Snapdragon_888_5G
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Gapps
+WITH_GMS := true
+WITH_GMS_VARIANT := pico
